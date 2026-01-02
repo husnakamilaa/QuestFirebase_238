c4e4ae8efb2c8eee71b1fd0e88f5de3c2736243b
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-01-02 15:22:10 +0700

    feat: inisialisasi function getDataSiswa dan PostDataSiswa

---
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -0,0 +1,11 @@
+package com.example.firebase.repositori
+
+import com.example.firebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa() : List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
